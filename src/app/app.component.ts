a2ac72146d348b8bb7b88546a536598b4a4ed284
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-25 00:29:43 +0500

    initial commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoadingBarRouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
