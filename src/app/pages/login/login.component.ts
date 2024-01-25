39bda8e6e0ca20f4fda83d51bc682ec2c6b65777
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-25 14:45:33 +0500

    added the basic deisgn

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,25 +1,31 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CardComponent } from '../../shared/component/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, CardComponent],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, CardComponent, MatCardModule, MatInput, MatFormFieldModule, RouterLink, FormsModule],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  private router: Router = inject(Router);
   constructor(private toastrService: ToastrService, private _snackBar: MatSnackBar) {}
 
   onSubmit(){
-    this._snackBar.open('SUccess', 'drop', { verticalPosition : 'top'})
-
+    this._snackBar.open('Logged in!', 'X', {verticalPosition: 'top'})
+    this.router.navigate(['/messages']);
   }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { CardComponent } from '../../shared/component/card/card.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, CardComponent, MatCardModule, MatInput, MatFormFieldModule, RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
