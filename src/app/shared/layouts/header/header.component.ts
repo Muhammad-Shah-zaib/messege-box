43be4656e12463e9baff4b5bf2715dc245d39e76
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2024-01-26 15:43:58 +0500

    updated styling

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,5 +1,6 @@
+import { state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
@@ -9,8 +10,24 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [MatButtonModule, MatToolbarModule, MatIconModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
 
+  constructor() { }
+
+  toggelHeader( header: HTMLElement, btn: MatIconButton ): void {
+    // header.style.maxHeight = '1px'
+    btn._elementRef.nativeElement.classList.toggle('rotate-180')
+
+    if (header.style.maxHeight === '1px') {
+      header.style.maxHeight = '100px'
+    } else {
+      header.style.maxHeight = '1px'
+    }
+  }
 }
+function trigger(arg0: string, arg1: any[]): any {
+  throw new Error('Function not implemented.');
+}
+
